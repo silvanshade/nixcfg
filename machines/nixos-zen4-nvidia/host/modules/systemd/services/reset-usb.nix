c3475e5b9360f67398b9f81049d040058c0b623f
silvanshade <silvanshade@users.noreply.github.com>
2024-03-27 18:06:17 -0600

    WIP: disable reset-usb

--- machines/nixos-zen4-nvidia/host/modules/systemd/services/reset-usb.nix
+++ machines/nixos-zen4-nvidia/host/modules/systemd/services/reset-usb.nix
@@ -1,32 +1,32 @@
 { pkgs, ... }:
 {
-  systemd.services.usb-reset = {
-    description = "Reset USB devices on resume";
-    wantedBy = [
-      "post-resume.target"
-    ];
-    after = [
-      "post-resume.target"
-    ];
-    path = with pkgs; [
-      bash
-      coreutils
-      kmod
-      uhubctl
-    ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = pkgs.writeScript "reset-usb.sh" ''
-        #!/bin/sh
-        sleep 10
-        rmmod xhci_pci
-        rmmod xhci_hcd
-        sleep 5
-        modprobe xhci_pci
-        sleep 5
-        uhubctl --vendor 2188:0031 --action cycle
-      '';
-    };
-  };
+  # systemd.services.usb-reset = {
+  #   description = "Reset USB devices on resume";
+  #   wantedBy = [
+  #     "post-resume.target"
+  #   ];
+  #   after = [
+  #     "post-resume.target"
+  #   ];
+  #   path = with pkgs; [
+  #     bash
+  #     coreutils
+  #     kmod
+  #     uhubctl
+  #   ];
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     ExecStart = pkgs.writeScript "reset-usb.sh" ''
+  #       #!/bin/sh
+  #       sleep 10
+  #       rmmod xhci_pci
+  #       rmmod xhci_hcd
+  #       sleep 5
+  #       modprobe xhci_pci
+  #       sleep 5
+  #       uhubctl --vendor 2188:0031 --action cycle
+  #     '';
+  #   };
+  # };
 }
 
